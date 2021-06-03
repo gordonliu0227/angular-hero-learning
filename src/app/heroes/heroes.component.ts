72d697d44d2000550436f87ca52dc70aff77f150
Gordon <gordonliu0227@users.noreply.github.com>
2021-06-03 13:47:24 +1000

    add a dashboard and nav between different function

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,24 @@
-import { Hero } from './../hero';
 import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  constructor(private heroService: HeroService) { }
 
-  constructor(private heroService: HeroService) {}
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
+
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
